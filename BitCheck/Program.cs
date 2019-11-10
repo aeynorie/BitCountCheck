756ea6404304e997f388db045de78d3e3573e9b1
大橋礼典 <ayanori1215@gmail.com>
2019-11-11 02:26:39 +0900

    プロジェクト ファイルを追加します。

---
+++ BitCheck/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BitCheck
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ushort data = 0b0000000011111100;
+            Int32 maskResult = data & 0b1111111111111111;
+            Int32 resultXOR = CalculataionXOR(maskResult);
+            Int32 resultNOT = CalculationNOT(resultXOR);
+
+            BitArray bitArray = new BitArray(new int[] { resultNOT });
+
+            BitArray bitAreaData = DefineBitArea(bitArray);
+            int countMatchNum = CountMatchNum(bitAreaData);
+            Console.WriteLine(countMatchNum.ToString());
+        }
+
+        static Int32 CalculataionXOR(Int32 data)
+        {
+            Int32 resultXOR = (data >> 1) ^ data;
+            return resultXOR;
+        }
+
+
+        static Int32 CalculationNOT(Int32 data)
+        {
+            Int32 resultNOT = ~data;
+            return resultNOT;
+        } 
+
+        static BitArray DefineBitArea(BitArray data)
+        {
+            BitArray BitAreaData = new BitArray(8);
+            for (int i = 2; i<= 8; i++)
+            {               
+                for (int j = 0; j < BitAreaData.Length; j++)
+                {
+                    BitAreaData[j] = data[i];
+                }
+            }
+            return BitAreaData;
+        }
+
+        static int CountMatchNum(BitArray data)
+        {
+            int countMatch = 0;
+            for(int i = 0; i < data.Length; i++)
+            {
+                
+                if (data.Get(i) == true)
+                {
+                    countMatch++;
+                }
+            }
+            return countMatch;
+        }
+    }
+
+    
+}
